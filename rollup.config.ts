d6b30e749e73f8cd787cf3cad130d0d84c39ea27
rikka <admin@rikka.cc>
2022-08-16 16:48:14 +0800

    feat: 🎸 添加corejs

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,16 +1,20 @@
 import typescript from "rollup-plugin-typescript2";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 
 const config = [
-    {
-        input:"src/bin.ts",
-        output:{
-            format:"es",
-            banner:"#!/usr/bin/env node",
-            file:"./dist/bin.js"
-        },
-        plugins:[
-            typescript()
-        ]
-    }
-]
+  {
+    input: "src/bin.ts",
+    output: {
+      format: "es",
+      banner: "#!/usr/bin/env node",
+      file: "./dist/bin.js",
+    },
+    plugins: [
+      typescript(),
+      getBabelOutputPlugin({
+        presets: [["@babel/preset-env", { useBuiltIns: "usage", corejs: "3.24.1" }]],
+      }),
+    ],
+  },
+];
 export default config;
